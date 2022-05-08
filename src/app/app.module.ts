4d618d83c0495416d78ffb3600d5e6bb69511420
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-05-08 16:13:31 +0200

    submition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import {StudentComponent} from "./student/student.component";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CenterComponent } from './center/center.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    StudentComponent,
+    HeaderComponent,
+    FooterComponent,
+    CenterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
